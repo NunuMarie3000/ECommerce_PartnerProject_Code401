6ab2bf97322eae8d785e389b430ff9acd1109f51
Storm O'Bryant <vmarie1997@gmail.com>
2022-12-05 11:53:12 -0600

    scaffolded identity, added area, created models, need a database

---
+++ ECommerce_RetroGaming/Areas/Admin/Models/CategoryProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce_RetroGaming.Areas.Admin.Models
+{
+  public class CategoryProduct
+  {
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int CategoryId { get; set; }
+  }
+}
--- ECommerce_RetroGaming/Areas/Identity/Data/ECommerceDbContext.cs
+++ ECommerce_RetroGaming/Areas/Identity/Data/ECommerceDbContext.cs
@@ -1,22 +1,26 @@
-﻿using ECommerce_RetroGaming.Areas.Identity.Data;
+﻿using ECommerce_RetroGaming.Areas.Admin.Models;
+using ECommerce_RetroGaming.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce_RetroGaming.Areas.Identity.Data;
 
-public class ECommerceDbContext : IdentityDbContext<ApplicationUser>
+public class ECommerceDbContext: IdentityDbContext<ApplicationUser>
 {
-    public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options)
-        : base(options)
-    {
-    }
+  public DbSet<Category> Categories { get; set; }
+  public DbSet<Product> Products { get; set; }
+  public DbSet<CategoryProduct> CategoryProducts { get; set; }
+  public ECommerceDbContext( DbContextOptions<ECommerceDbContext> options )
+      : base(options)
+  {
+  }
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
-    }
+  protected override void OnModelCreating( ModelBuilder builder )
+  {
+    base.OnModelCreating(builder);
+    // Customize the ASP.NET Identity model and override the defaults if needed.
+    // For example, you can rename the ASP.NET Identity table names and more.
+    // Add your customizations after calling base.OnModelCreating(builder);
+  }
 }
